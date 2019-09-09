41fe3eb9ad60b4140c826ada2e02f00f211cb870
tavisca-cchowdhary <cchowdhary@tavisca.in>
2019-09-09 11:06:47 +0530

    Implemented spring

---
+++ spring_sample_xml/src/main/java/Application.java
@@ -0,0 +1,18 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.tavisca.service.CustomerService;
+
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//CustomerService service = new CustomerServiceImpl();
+		
+		ApplicationContext appContext = new ClassPathXmlApplicationContext("applicationContext.xml");
+		
+		CustomerService service = appContext.getBean("customerService",CustomerService.class);
+		
+		System.out.println(service.findAll().get(0).getFirstname());
+		}
+}
---
+++ spring_sample_xml/src/main/java/com/tavisca/service/CustomerService.java
@@ -0,0 +1,11 @@
+package com.tavisca.service;
+
+import java.util.List;
+
+import com.tavisca.modal.Customer;
+
+public interface CustomerService {
+
+	List<Customer> findAll();
+
+}
