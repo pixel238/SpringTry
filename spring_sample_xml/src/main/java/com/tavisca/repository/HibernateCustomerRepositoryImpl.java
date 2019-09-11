18e14d07e223d9733dbf3f65ebe9d9fed84bc859
tavisca-cchowdhary <cchowdhary@tavisca.in>
2019-09-11 14:00:25 +0530

    Added properties as seperate file

--- spring_sample_xml/src/main/java/com/tavisca/repository/HibernateCustomerRepositoryImpl.java
+++ spring_sample_xml/src/main/java/com/tavisca/repository/HibernateCustomerRepositoryImpl.java
@@ -3,10 +3,17 @@ package com.tavisca.repository;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Value;
+
 import com.tavisca.modal.Customer;
 
 public class HibernateCustomerRepositoryImpl implements CustomerRepository {
+	@Value("${dbUsername}")
+	public String dbUsername;
+
 	public List<Customer> findAll(){
+		System.out.println(dbUsername);
+		
 		List<Customer> customers = new ArrayList<Customer>();
 		
 		Customer customer = new Customer();
